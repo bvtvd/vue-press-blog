89e0934f56ce66b926a51e41a868a94ce61e2e59
hans <hans01@foxmail.com>
2022-05-06 16:20:21 +0800

    steps

---
+++ deploy.bat
@@ -0,0 +1,10 @@
+cd /docs/.vuepress/dist
+
+git init
+
+git remote add origin git@github.com:bvtvd/bvtvd.github.io.git
+
+git branch -M main
+git add .
+git commit -m 'steps'
+git push -u origin main
