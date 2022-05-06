e636fa992941334705d93df9025d953b3d18ce74
hans <hans01@foxmail.com>
2022-05-06 15:48:17 +0800

    first commit

---
+++ docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from 'D:/www/vuepress_starter/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]
